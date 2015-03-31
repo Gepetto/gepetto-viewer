9a8608acc19df3bd4f9ae64b828e5423255fb0bc
Joseph Mirabel <jmirabel@laas.fr>
2015-03-31 14:41:25 +0200

    Add arrows around a graphics::Node.

--- src/node.cpp
+++ src/node.cpp
@@ -8,13 +8,55 @@
 
 #include <gepetto/viewer/node.h>
 #include <osg/Material>
+#include <osg/LineWidth>
 #include <climits>
 
 namespace graphics {
 
+  bool Node::firstTime_ = true;
+  ::osg::NodeRefPtr Node::arrow_ = 0;
+  ::osg::NodeRefPtr Node::circle_ = 0;
+
   /* Declaration of private function members */
   void Node::init ()
   {
+    if (firstTime_) {
+      const float factor = 0.1;
+      ::osg::GroupRefPtr group = new ::osg::Group;
+      ::osg::GeodeRefPtr cylinder = new osg::Geode;
+      cylinder->addDrawable (new osg::ShapeDrawable (new osg::Cylinder (osg::Vec3(0,0,0), 1*factor, 4*factor)));
+      group->addChild (cylinder);
+      ::osg::GeodeRefPtr cone = new osg::Geode;
+      cone->addDrawable (new osg::ShapeDrawable (new osg::Cone (osg::Vec3(0,0,0), 1.8*factor, 2*factor)));
+      ::osg::PositionAttitudeTransformRefPtr cone_transform = new ::osg::PositionAttitudeTransform;
+      cone_transform->addChild (cone);
+      cone_transform->setPosition (osg::Vec3 (0,0,2*factor));
+      group->addChild (cone_transform);
+      arrow_ = group;
+      ::osg::GeometryRefPtr circle = new ::osg::Geometry;
+      ::osg::ref_ptr< ::osg::Vec3Array> v = new ::osg::Vec3Array;
+      const float radius = 1; const int POLYGON_SIZE = 256;
+      double theta, px, py;
+      for (int i = 1; i <= POLYGON_SIZE; ++i) {
+        theta = 2.0 * osg::PI  / POLYGON_SIZE * i;
+        px = radius * cos (theta);
+        py = radius * sin (theta);
+        v->push_back (osg::Vec3(px,py,0));
+      }
+      circle->setVertexArray (v.get());
+      circle->addPrimitiveSet (
+          new ::osg::DrawArrays (::osg::PrimitiveSet::LINE_LOOP, 0, POLYGON_SIZE));
+      ::osg::StateSet* stateset = new ::osg::StateSet;
+      ::osg::LineWidth* linewidth = new ::osg::LineWidth;
+      linewidth->setWidth (2.0f);
+      stateset->setAttributeAndModes (linewidth, osg::StateAttribute::ON);
+      circle->setStateSet (stateset);
+      ::osg::GeodeRefPtr circleNode = new ::osg::Geode;
+      circleNode->addDrawable (circle.get());
+      circle_ = circleNode;
+      firstTime_ = false;
+    }
+
     /* Build local conformation
        <- wireframe_node_ptr_
        connected to the parent <- switch_node_ptr_                           <- auto_transform_ptr_ <- connection of children here
@@ -23,6 +65,7 @@ namespace graphics {
 
     auto_transform_ptr_ = new ::osg::AutoTransform;
     static_auto_transform_ptr_ = new ::osg::AutoTransform;
+    arrows_transform_ptr_ = new ::osg::PositionAttitudeTransform;
     switch_node_ptr_ = new ::osg::Switch;
     switch_node_ptr_->setName (id_name_);
 
@@ -158,6 +201,87 @@ namespace graphics {
 
     default:
       ASSERT(false, "WireFrameMode set with bad option");
+      break;
+    }
+  }
+
+  void Node::selectArrows (int arrows)
+  {
+    auto_transform_ptr_->removeChild(arrows_transform_ptr_);
+    arrows_transform_ptr_ = new ::osg::PositionAttitudeTransform;
+    auto_transform_ptr_->addChild(arrows_transform_ptr_);
+    float radius = static_auto_transform_ptr_->getBound ().radius();
+    ::osg::PositionAttitudeTransformRefPtr axis = 0;
+    if (arrows & TRANS_X) {
+      axis = new ::osg::PositionAttitudeTransform;
+      axis->setPosition (osg::Vec3(radius, 0, 0));
+      axis->setAttitude (osg::Quat(osg::PI_2, osg::Vec3(0, 1, 0)));
+      axis->setScale (osg::Vec3 (radius, radius, radius));
+      axis->addChild (arrow_);
+      arrows_transform_ptr_->addChild (axis);
+    }
+    if (arrows & TRANS_Y) {
+      axis = new ::osg::PositionAttitudeTransform;
+      axis->setPosition (osg::Vec3(0, radius, 0));
+      axis->setAttitude (osg::Quat(-osg::PI_2, osg::Vec3(1, 0, 0)));
+      axis->setScale (osg::Vec3 (radius, radius, radius));
+      axis->addChild (arrow_);
+      arrows_transform_ptr_->addChild (axis);
+    }
+    if (arrows & TRANS_Z) {
+      axis = new ::osg::PositionAttitudeTransform;
+      axis->setPosition (osg::Vec3(0, 0, radius));
+      axis->setScale (osg::Vec3 (radius, radius, radius));
+      axis->addChild (arrow_);
+      arrows_transform_ptr_->addChild (axis);
+    }
+    if (arrows & ROT_X) {
+      axis = new ::osg::PositionAttitudeTransform;
+      axis->setScale (osg::Vec3 (radius, radius, radius));
+      axis->setAttitude (osg::Quat(osg::PI_2, osg::Vec3(0, 1, 0)));
+      axis->addChild (circle_);
+      arrows_transform_ptr_->addChild (axis);
+    }
+    if (arrows & ROT_Y) {
+      axis = new ::osg::PositionAttitudeTransform;
+      axis->setScale (osg::Vec3 (radius, radius, radius));
+      axis->setAttitude (osg::Quat(-osg::PI_2, osg::Vec3(1, 0, 0)));
+      axis->addChild (circle_);
+      arrows_transform_ptr_->addChild (axis);
+    }
+    if (arrows & ROT_Z) {
+      axis = new ::osg::PositionAttitudeTransform;
+      axis->setScale (osg::Vec3 (radius, radius, radius));
+      axis->addChild (circle_);
+      arrows_transform_ptr_->addChild (axis);
+    }
+    setArrowsVisibility (VISIBILITY_OFF);
+  }
+
+  void Node::setArrowsVisibility (const VisibilityMode& mode)
+  {
+    switch (mode) {
+    case VISIBILITY_ON:
+      arrows_transform_ptr_->setNodeMask(0xffffffff);
+      arrows_transform_ptr_->getOrCreateStateSet()->setRenderBinToInherit();
+      arrows_transform_ptr_->getOrCreateStateSet()->setMode(GL_DEPTH_TEST, ::osg::StateAttribute::ON | ::osg::StateAttribute::PROTECTED);
+      arrows_transform_ptr_->getOrCreateStateSet()->setMode(GL_CULL_FACE,::osg::StateAttribute::OFF | ::osg::StateAttribute::PROTECTED);
+      break;
+    case VISIBILITY_OFF:
+      arrows_transform_ptr_->setNodeMask(0x0);
+      arrows_transform_ptr_->getOrCreateStateSet()->setRenderBinToInherit();
+      arrows_transform_ptr_->getOrCreateStateSet()->setMode(GL_DEPTH_TEST, ::osg::StateAttribute::ON | ::osg::StateAttribute::PROTECTED);
+      arrows_transform_ptr_->getOrCreateStateSet()->setMode(GL_CULL_FACE,::osg::StateAttribute::OFF | ::osg::StateAttribute::PROTECTED);
+      break;
+    case ALWAYS_ON_TOP:
+      arrows_transform_ptr_->setNodeMask(0xffffffff);
+      arrows_transform_ptr_->getOrCreateStateSet()->setRenderBinDetails(INT_MAX, "DepthSortedBin");
+      arrows_transform_ptr_->getOrCreateStateSet()->setMode(GL_DEPTH_TEST, ::osg::StateAttribute::OFF | ::osg::StateAttribute::PROTECTED);
+      arrows_transform_ptr_->getOrCreateStateSet()->setMode(GL_CULL_FACE, ::osg::StateAttribute::ON | ::osg::StateAttribute::PROTECTED );
+      break;
+
+    default:
+      ASSERT(false, "mode is not well defined");
       break;
     }
   }
