fffaae1c8f73ebf5f82e811e28a1fd6301118727
Florent Lamiraux <florent@laas.fr>
2016-10-26 15:37:27 +0200

    In LeafNodeFace::setTexture, add mapping of the texture on the face
      specify that each corner of the image is on a point of the face.

--- src/leaf-node-face.cpp
+++ src/leaf-node-face.cpp
@@ -189,6 +189,13 @@ namespace graphics {
         return;
       } 
       texture->setImage(image);
+      osg::Vec2Array* texcoords = new osg::Vec2Array(4);
+      (*texcoords)[0].set(0.00f,0.0f); // texture coord for vertex 0
+      (*texcoords)[1].set(1.00f,0.0f); // texture coord for vertex 1
+      (*texcoords)[2].set(1.00f,1.0f); // texture coord for vertex 2
+      (*texcoords)[3].set(0.00f,1.0f); // texture coord for vertex 3
+      face_ptr_->setTexCoordArray(0,texcoords);
+
       geode_ptr_->getStateSet()->setTextureAttributeAndModes(0,texture,osg::StateAttribute::ON);
     }
     
