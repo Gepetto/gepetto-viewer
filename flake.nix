467743d2d7afa4ffa0248ede72730059c1e06ae3
Guilhem Saurel <guilhem.saurel@laas.fr>
2025-10-19 20:25:05 +0200

    nix: sync with gepetto/nix template

--- flake.nix
+++ flake.nix
@@ -2,20 +2,30 @@
   description = "An user-friendly Graphical Interface";
 
   inputs = {
-    flake-parts.url = "github:hercules-ci/flake-parts";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    gepetto.url = "github:gepetto/nix";
+    flake-parts.follows = "gepetto/flake-parts";
+    nixpkgs.follows = "gepetto/nixpkgs";
+    nix-ros-overlay.follows = "gepetto/nix-ros-overlay";
+    systems.follows = "gepetto/systems";
+    treefmt-nix.follows = "gepetto/treefmt-nix";
   };
 
   outputs =
     inputs:
     inputs.flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = inputs.nixpkgs.lib.systems.flakeExposed;
+      systems = import inputs.systems;
+      imports = [ inputs.gepetto.flakeModule ];
       perSystem =
-        { pkgs, self', ... }:
+        {
+          lib,
+          pkgs,
+          self',
+          ...
+        }:
         {
           packages = {
             default = self'.packages.gepetto-viewer;
-            gepetto-viewer = pkgs.python3Packages.gepetto-viewer.overrideAttrs (_: {
+            gepetto-viewer = pkgs.python3Packages.gepetto-viewer.overrideAttrs {
               src = pkgs.lib.fileset.toSource {
                 root = ./.;
                 fileset = pkgs.lib.fileset.unions [
@@ -32,7 +42,7 @@
                   ./tests
                 ];
               };
-            });
+            };
           };
         };
     };
