031ee43e7d5bbd0232a646394a2e2e67a62ea2d2
Guilhem Saurel <guilhem.saurel@laas.fr>
2026-01-13 15:46:40 +0100

    CMake: drop Qt4

--- cmake-module/osgqt.cmake
+++ cmake-module/osgqt.cmake
@@ -1,53 +1,17 @@
+# osgQt .pc filename change all the time.
 macro(ADD_OSGQT_DEPENDENCY)
-  if(${OPENSCENEGRAPH_VERSION} VERSION_GREATER "3.5.5")
-    if(NOT DEFINED PROJECT_USE_QT4)
-      set(PROJECT_USE_QT4
-          FALSE
-          CACHE BOOL "Use Qt4 instead of Qt5")
-    endif(NOT DEFINED PROJECT_USE_QT4)
-    if(PROJECT_USE_QT4)
-      set(OSGQT_DEPENDENCY openscenegraph-osgQt)
-    else(PROJECT_USE_QT4)
-      set(OSGQT_DEPENDENCY openscenegraph-osgQt5)
-    endif(PROJECT_USE_QT4)
+  set(OSGQT_DEPENDENCY openscenegraph-osgQt)
+  find_package(${OSGQT_DEPENDENCY} QUIET)
+  if(${OSGQT_DEPENDENCY}_FOUND)
     add_required_dependency(${OSGQT_DEPENDENCY})
   else()
-    set(OSGQT_DEPENDENCY openscenegraph-osgQt)
-    add_required_dependency(${OSGQT_DEPENDENCY})
-
-    if(NOT DEFINED PROJECT_USE_QT4)
-      if(EXISTS "${OPENSCENEGRAPH_OSGQT_INCLUDEDIR}/osgQt/Version")
-        file(READ "${OPENSCENEGRAPH_OSGQT_INCLUDEDIR}/osgQt/Version"
-             osgqt_version_content LIMIT 500)
-        string(FIND ${osgqt_version_content} "#define OSGQT_QT_VERSION 4"
-                    osgqt_qt_version4_idx)
-        string(FIND ${osgqt_version_content} "#define OSGQT_QT_VERSION 5"
-                    osgqt_qt_version5_idx)
-        if(osgqt_qt_version4_idx EQUAL -1 AND osgqt_qt_version5_idx EQUAL -1)
-
-        elseif(osgqt_qt_version4_idx EQUAL -1)
-          # QT5 only
-          set(PROJECT_USE_QT4
-              FALSE
-              CACHE BOOL "Use Qt4 instead of Qt5")
-        elseif(osgqt_qt_version5_idx EQUAL -1)
-          # QT4 only
-          set(PROJECT_USE_QT4
-              TRUE
-              CACHE BOOL "Use Qt4 instead of Qt5")
-        else()
-          # Both found.
-          message(
-            FATAL_ERROR
-              "Could not compute Qt version. Please set PROJECT_USE_QT4 variable manually"
-          )
-        endif()
-      else()
-        # QT4 only
-        set(PROJECT_USE_QT4
-            TRUE
-            CACHE BOOL "Use Qt4 instead of Qt5")
-      endif()
-    endif(NOT DEFINED PROJECT_USE_QT4)
+    set(OSGQT_DEPENDENCY openscenegraph-osgQt6)
+    find_package(${OSGQT_DEPENDENCY} QUIET)
+    if(${OSGQT_DEPENDENCY}_FOUND)
+      add_required_dependency(${OSGQT_DEPENDENCY})
+    else()
+      set(OSGQT_DEPENDENCY openscenegraph-osgQt5)
+      add_required_dependency(${OSGQT_DEPENDENCY})
+    endif()
   endif()
 endmacro(ADD_OSGQT_DEPENDENCY)
