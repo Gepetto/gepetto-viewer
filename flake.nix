d350f18bec5ceb07bce35ec61d918b7a80a7ceec
Guilhem Saurel <guilhem.saurel@laas.fr>
2026-04-22 20:44:58 +0200

    sync with gepetto/nix template

--- flake.nix
+++ flake.nix
@@ -1,48 +1,32 @@
 {
   description = "An user-friendly Graphical Interface";
 
-  inputs = {
-    gepetto.url = "github:gepetto/nix";
-    flakoboros.follows = "gepetto/flakoboros";
-    gazebros2nix.follows = "gepetto/gazebros2nix";
-    flake-parts.follows = "gepetto/flake-parts";
-    nixpkgs.follows = "gepetto/nixpkgs";
-    systems.follows = "gepetto/systems";
-    treefmt-nix.follows = "gepetto/treefmt-nix";
-  };
+  inputs.gepetto.url = "github:gepetto/nix";
 
   outputs =
     inputs:
-    inputs.flake-parts.lib.mkFlake { inherit inputs; } (
+    inputs.gepetto.lib.mkFlakoboros inputs (
       { lib, ... }:
       {
-        systems = import inputs.systems;
-        imports = [
-          inputs.gepetto.flakeModule
-          {
-            flakoboros = {
-              pyOverrideAttrs.gepetto-viewer = _: _: { };
-              overrideAttrs.gepetto-viewer = _: {
-                src = lib.fileset.toSource {
-                  root = ./.;
-                  fileset = lib.fileset.unions [
-                    ./cmake-module
-                    ./CMakeLists.txt
-                    ./doc
-                    ./fonts
-                    ./include
-                    ./package.xml
-                    ./plugins
-                    ./pyplugins
-                    ./res
-                    ./src
-                    ./tests
-                  ];
-                };
-              };
-            };
-          }
-        ];
+        extraDevPyPackages = [ "gepetto-viewer" ];
+        overrideAttrs.gepetto-viewer = {
+          src = lib.fileset.toSource {
+            root = ./.;
+            fileset = lib.fileset.unions [
+              ./cmake-module
+              ./CMakeLists.txt
+              ./doc
+              ./fonts
+              ./include
+              ./package.xml
+              ./plugins
+              ./pyplugins
+              ./res
+              ./src
+              ./tests
+            ];
+          };
+        };
       }
     );
 }
